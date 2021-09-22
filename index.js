a6065fc1df32b5f7381ffd5c1ca46af3bde36405
Sarah <spfohman@email.essex.edu>
2021-09-22 12:15:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Add your code here
+function submitData(name, email){
+    const data = {
+
+        name: name,
+        email:email
+    };
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type":"application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify(data),
+};
+    
+    return fetch("http://localhost:3000/users", configObj)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(object){
+      
+       let info = document.createElement('ul');
+       info.innerHTML=`
+        <li> ${object['id']}</li>
+        <li> ${object['name']}</li>
+        <li>${object['email']}</li>`
+        document.querySelector('body').appendChild(info);
+    })
+    .catch(function(message){
+        let msg = document.createElement('ul');
+        msg.innerHTML=`
+        <p>${message}</p>`
+        document.querySelector('body').appendChild(msg);
+        //alert("Bad Data!");
+        //console.log(error.message);
+    });
+
+}
